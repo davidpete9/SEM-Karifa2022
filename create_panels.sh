efaaf7211a0c6748022898ca1a6ebfc620b14dc5
David Pete <dervid9@gmail.com>
2022-10-27 20:51:25 +0200

    javitasok

--- create_panels.sh
+++ create_panels.sh
@@ -3,7 +3,8 @@
     --tabs 'annotation' \
     --framing 'frame; width: 5mm; space: 3mm; cuts: h' \
     --cuts 'mousebites; drill: 0.5mm; spacing: 1mm; offset: 0.2mm; prolong: 0.5mm' \
-    --post 'millradius: 0.5mm' \
+   --source 'tolerance: 20mm' \
+   --post 'millradius: 0.5mm; copperfill: true' \
     karifa.kicad_pcb panel.kicad_pcb
     
 #--tabs 'fixed; width: 3mm; height: 3mm; vcount: 1; hcount: 1' \
