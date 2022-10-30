8b0f023769b880aed8de9b07b7da85beddf29776
Hekk_Elek <horvath.kristof.sz@gmail.com>
2022-10-30 20:12:27 +0100

    Sent to fab.

--- create_panels.sh
+++ create_panels.sh
@@ -2,7 +2,7 @@
     --layout 'grid; rows: 3; cols: 7; vspace: 5mm; hspace: -9mm; alternation: rowsCols; rotation: 0deg; hbackbone: 5mm;' \
     --tabs 'annotation' \
     --framing 'frame; width: 5mm; space: 3mm; cuts: h' \
-    --cuts 'mousebites; drill: 0.5mm; spacing: 1mm; offset: 0.2mm; prolong: 0.5mm' \
+    --cuts 'mousebites; drill: 0.5mm; spacing: 1mm; offset: -0.5mm; prolong: 0.5mm' \
    --source 'tolerance: 20mm' \
    --post 'millradius: 0.5mm; copperfill: true' \
     karifa.kicad_pcb panel.kicad_pcb
