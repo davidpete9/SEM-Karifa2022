0149182a162a6acf83527bcd7e459769b4e0232f
David Pete <dervid9@gmail.com>
2022-11-09 22:44:18 +0100

    hullocsillag

--- create_panels.sh
+++ create_panels.sh
@@ -1,9 +1,10 @@
   kikit panelize \
-    --layout 'grid; rows: 2; cols: 4; vspace: 5mm; hspace: 0mm; alternation: rows; rotation: 90deg; hbackbone: 5mm;' \
+    --layout 'grid; rows: 2; cols: 4; vspace: 4.5mm; hspace: 0mm; alternation: rows; rotation: 90deg; hbackbone: 5mm;' \
     --tabs 'annotation' \
     --framing 'frame; width: 5mm; space: 3mm; cuts: h' \
+    --source 'tolerance: 20mm' \
     --cuts 'mousebites; drill: 0.5mm; spacing: 1mm; offset: 0.2mm; prolong: 0.5mm' \
-    --post 'millradius: 0.5mm' \
+    --post 'millradius: 0.5mm; copperfill: true' \
     karifa.kicad_pcb panel.kicad_pcb
     
 #--tabs 'fixed; width: 3mm; height: 3mm; vcount: 1; hcount: 1' \
